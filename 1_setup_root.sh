71ffb065ce2bc807133ca7e99d1207449ce0cf4d
Joerg <joerg@neulist.de>
2022-01-03 18:15:57 +0100

    Add plotly widgets and their bizarre dependencies on npm and nodejs

--- 1_setup_root.sh
+++ 1_setup_root.sh
@@ -6,7 +6,8 @@ pacman-key --init
 pacman-key --populate
 pacman-key --refresh-keys
 pacman -Sy archlinux-keyring
-pacman -Syu base-devel git jupyterlab python-pip python-virtualenv $PACKAGES
+pacman -Syu base-devel git jupyterlab nodejs npm python-pip python-virtualenv $PACKAGES
+jupyter labextension install jupyterlab-plotly @jupyter-widgets/jupyterlab-manager plotlywidget
 
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 useradd -m -G wheel -s $USER_SHELL $LIN_USER
