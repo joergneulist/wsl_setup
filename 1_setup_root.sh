cbc932d4f6b058b90e06142b9d8c5a487e9e92da
Jörg Neulist <21063011+joergneulist@users.noreply.github.com>
2022-02-17 21:55:06 +0100

    Add jupyterlab-widgets and ipywidgets packages for plotly

--- 1_setup_root.sh
+++ 1_setup_root.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+python-ipywidgets jupyterlab-widgets#!/bin/bash
 
 source $(dirname $0)/config
 
@@ -6,7 +6,7 @@ pacman-key --init
 pacman-key --populate
 pacman-key --refresh-keys
 pacman -Sy archlinux-keyring
-pacman -Syu base-devel git jupyterlab nodejs npm python-pip python-virtualenv xorg-xauth $PACKAGES
+pacman -Syu base-devel git jupyterlab jupyterlab-widgets nodejs npm python-ipywidgets python-pip python-virtualenv xorg-xauth $PACKAGES
 jupyter labextension install jupyterlab-plotly @jupyter-widgets/jupyterlab-manager plotlywidget
 
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
