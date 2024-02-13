c2294e2e37c7a3937bccbb31263bf1a8a44a8b19
Joerg <joerg.neulist@appsphere.de>
2024-02-13 22:36:35 +0100

    add chsh command and fix some minor issues

--- .install_root.sh
+++ .install_root.sh
@@ -3,3 +3,4 @@
 apt update
 apt dist-upgrade -y
 apt install -y bat exa python3-poetry zsh
+chsh $1 -s /usr/bin/zsh
--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 BASE=$(readlink -f $(dirname $0))
-sudo $BASE/.install_root.sh
-
 source $BASE/config
 
+echo '###################### UPDATE SYSTEM'
+sudo $BASE/.install_root.sh $(whoami)
+ln -s /mnt/c/users/$WIN_USER ~/winhome
+
+echo '###################### CONFIGURE GIT'
 git config --global credential.helper store
 git config --global pull.rebase true
 git config --global user.email $USER_EMAIL
 git config --global user.name $USER_NAME
 
-ln -s /mnt/c/users/$WIN_USER ~/winhome
-
-install -D -t ~/.jupyter $BASE/files/jupyter_lab_config.py
-
+echo '###################### CONFIGURE JUPYTER'
 poetry new $JUPY_DIR
 pushd $JUPY_DIR
 poetry add ipykernel jupyter pandas $JUPYTER_EXT
 poetry install
+install -D -t ~/.jupyter $BASE/files/jupyter_lab_config.py
 popd
 
+echo '###################### INSTALL OH-MYZSH'
 INSTALLER=$(mktemp)
 curl -fsSLo $INSTALLER https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 chmod a+x $INSTALLER
 ZSH=$OMZ_DIR $INSTALLER --unattended
-echo "for f in $BASE/.env.d/*; do source \$f; done" > $OMZ_DIR/custom/custom_env.zsh
+echo "for f in $BASE/.env.d/*; do source \$f; done" > $OMZ_DIR/custom/env.zsh
 
 #IP==$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')
 #touch ~/.Xauthority
